e1a1ed162e5f5fcbb01802c15301d953c130c993
Prince1895 <chauhanprince21153366@gmail.com>
2025-08-08 01:07:09 +0530

    add some checks

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext'; // Import AuthProvider
-
+console.log('VITE_API_BASE_URL:', import.meta.env.VITE_API_BASE_URL);
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <AuthProvider> {/* Wrap App with AuthProvider */}
